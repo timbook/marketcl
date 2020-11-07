16cf211937a6b8c0fa2d93ee73fefa506e3af7d4
Tim Book <timothykbook@gmail.com>
2020-11-07 01:42:03 -0500

    added sroc and wr

--- marketcl/plots/plotter.py
+++ marketcl/plots/plotter.py
@@ -11,7 +11,9 @@ class Plotter:
 
     def plot(self, choice):
         plot_dispatch = {
-            "macd": self.plot_macd
+            "macd": self.plot_macd,
+            "sroc": self.plot_sroc,
+            "wr": self.plot_wr
         }
         plot_dispatch[choice]()
 
@@ -45,5 +47,47 @@ class Plotter:
         axs[1][1].vlines(self.data.index, ymin=0, ymax=macd_hist, color=color)
 
         axs[0][1].legend()
+        plt.tight_layout()
+        plt.show()
+
+    def plot_sroc(self):
+        ema = self.data.close.ewm(span=13).mean()
+        ema_shift = ema.shift(21)
+        roc = ema / ema_shift
+        centerline = (roc.max() + roc.min()) / 2
+        meanline = roc.mean()
+
+        fig, axs = plt.subplots(3, 1, figsize=(14, 8))
+
+        self.plot_price(axs[0])
+        self.plot_volume(axs[1])
+
+        axs[2].set_title("Smoothed Rate of Change", loc="left")
+        axs[2].plot(self.data.index, roc, color="navy")
+        axs[2].axhline(centerline, color="black")
+
+        plt.tight_layout()
+        plt.show()
+
+    def plot_wr(self):
+        h7 = self.data.high.rolling(7).max()
+        l7 = self.data.low.rolling(7).min()
+        wr = (h7 - self.data.close) / (h7 - l7)
+
+        U = wr.max()
+        L = wr.min()
+        high_ref = U - (U - L)*0.10
+        low_ref = L + (U - L)*0.10
+
+        fig, axs = plt.subplots(3, 1, figsize=(14, 8))
+
+        self.plot_price(axs[0])
+        self.plot_volume(axs[1])
+
+        axs[2].set_title("Williams %R", loc="left")
+        axs[2].plot(self.data.index, wr, color="navy")
+        axs[2].axhline(high_ref, color="black")
+        axs[2].axhline(low_ref, color="black")
+
         plt.tight_layout()
         plt.show()
